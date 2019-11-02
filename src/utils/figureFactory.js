e1ed3c46ca40f0ae26588517c701f6a1a2ca75bb
Alex Stanoev <stanoev1337@gmail.com>
2019-11-03 00:36:51 +0200

    change swith with if in figureFactory.js

--- src/utils/figureFactory.js
+++ src/utils/figureFactory.js
@@ -7,22 +7,24 @@ import Queen from '../figures/Queen';
 import King from '../figures/King';
 
 export default (name, color, col, row, finalPoint) => {
-    const figureProps = [color, col, row]
-    switch (name) {
-        case constants.QUEEN:
-            return new Queen(...figureProps);
-        case constants.KING:
-            return new King(...figureProps);
-        case constants.BISHOP:
-            return new Bishop(...figureProps);
-        case constants.KNIGHT:
-            return new Knight(...figureProps); 
-        case constants.ROOK:
-            return new Rook(...figureProps);
-        case constants.PAWN:
-            return new Pawn(...figureProps, finalPoint);
-        default:
-            return new Error("Wrong figure name");
-
+    const figureProps = [color, col, row];
+    if (name === constants.QUEEN) {
+        return new Queen(...figureProps);
     }
-}
+    if (name === constants.KING) {
+        return new King(...figureProps);
+    }
+    if (name === constants.BISHOP) {
+        return new Bishop(...figureProps);
+    }
+    if (name === constants.KNIGHT) {
+        return new Knight(...figureProps);
+    }
+    if (name === constants.ROOK) {
+        return new Rook(...figureProps);
+    }
+    if (name === constants.PAWN) {
+        return new Pawn(...figureProps, finalPoint);
+    }
+    return new Error('Wrong figure name');
+};
